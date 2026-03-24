4dc11596cbc1baecfb4a907a54bf4ccfc0715a09
Stellaksiina <stellaksiina@gmail.com>
2026-03-24 22:09:38 +0200

    Lisää tehtävät.

---
+++ Module_10/Tehtävä_3.py
@@ -0,0 +1,35 @@
+class Elevator:
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def go_to_floor(self, new_floor):
+        while new_floor > self.current_floor:
+            self.floor_up()
+        while new_floor < self.current_floor:
+            self.floor_down()
+
+    def floor_up(self):
+        self.current_floor += 1
+
+    def floor_down(self):
+        self.current_floor -= 1
+
+
+class Building:
+    def __init__(self, bottom_floor, top_floor, elevator_count):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.elevators = []
+
+        for _ in range(elevator_count):
+            elevator = Elevator(bottom_floor, top_floor)
+            self.elevators.append(elevator)
+
+    def run_elevator(self, elevator_number, target_floor):
+        self.elevators[elevator_number].go_to_floor(target_floor)
+
+    def fire_alarm(self):
+        for elevator in self.elevators:
+            elevator.go_to_floor(self.bottom_floor)
