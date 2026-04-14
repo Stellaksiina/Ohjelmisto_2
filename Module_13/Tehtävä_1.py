7116f9c7f119eb37dbb71f44cd1477e4066c06c0
Stellaksiina <stellaksiina@gmail.com>
2026-04-14 23:36:52 +0300

    Lisää tehtävät.

---
+++ Module_13/Tehtävä_1.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+@app.route('/alkuluku/<int:number>', methods=['GET'])
+def check_prime(number):
+    return jsonify({
+        "Number": number,
+        "isPrime": is_prime(number)
+    })
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host="127.0.0.1", port=3000)
+
+#En ymmärtäny tästä yhtää mitää whatafak
